1472024b15de352fbfae0eb55eb5238aaa9eac0f
chanha-ryu <chanha.ryu@samsung.com>
2024-11-19 17:11:20 +0900

    add awating list. remove reload.

--- content.js
+++ content.js
@@ -108,86 +108,112 @@ const macroStop = () => {
     reload();
 };
 
+function waitForElm(selector) {
+    return new Promise((resolve) => {
+        if (document.querySelector(selector)) {
+            return resolve(document.querySelector(selector));
+        }
+
+        const observer = new MutationObserver((mutations) => {
+            if (document.querySelector(selector)) {
+                observer.disconnect();
+                resolve(document.querySelector(selector));
+            }
+        });
+
+        // If you get "parameter 1 is not of type 'Node'" error, see https://stackoverflow.com/a/77855838/492336
+        observer.observe(document.body, {
+            childList: true,
+            subtree: true,
+        });
+    });
+}
+
 const macro = () => {
-    let uid = 0;
-    let $row;
-    const $rows = document.querySelectorAll("#tableResult > tbody > tr");
-    const len = $rows.length;
+    waitForElm("#tableResult > tbody > tr").then(() => {
+        let uid = 0;
+        let $row;
+        const $rows = document.querySelectorAll("#tableResult > tbody > tr");
+        const len = $rows.length;
 
-    if (!len) {
-        return;
-    }
+        if (!len) {
+            return;
+        }
 
-    for (let i = 0; i < len; i++) {
-        $row = $rows[i];
+        for (let i = 0; i < len; i++) {
+            $row = $rows[i];
 
-        if (isChecked(++uid)) {
-            $row.querySelector("td:nth-child(5)").style.backgroundColor =
-                "#f03e3e";
-            const $button =
-                $row
-                    .querySelector("td:nth-child(5)")
+            if (isChecked(++uid)) {
+                $row.querySelector("td:nth-child(5)").style.backgroundColor =
+                    "#f03e3e";
+                const $button =
+                    $row
+                        .querySelector("td:nth-child(5)")
+                        .querySelector(
+                            '[src="/docs/2007/img/common/icon_apm_bl.gif"]'
+                        ) ||
+                    $row
+                        .querySelector("td:nth-child(5)")
+                        .querySelector(
+                            '[src="/docs/2007/img/common/icon_apm_rd.gif"]'
+                        );
+
+                if ($button) {
+                    $button.closest("a").click();
+                    sessionStorage.removeItem("macro_start");
+                    chrome.extension.sendMessage({ type: "successTicketing" });
+                    break;
+                }
+            }
+
+            if (isChecked(++uid)) {
+                $row.querySelector("td:nth-child(6)").style.backgroundColor =
+                    "#f03e3e";
+                const $button =
+                    $row
+                        .querySelector("td:nth-child(6)")
+                        .querySelector(
+                            '[src="/docs/2007/img/common/icon_apm_bl.gif"]'
+                        ) ||
+                    $row
+                        .querySelector("td:nth-child(6)")
+                        .querySelector(
+                            '[src="/docs/2007/img/common/icon_apm_rd.gif"]'
+                        );
+
+                if ($button) {
+                    $button.closest("a").click();
+                    sessionStorage.removeItem("macro_start");
+                    chrome.extension.sendMessage({ type: "successTicketing" });
+                    break;
+                }
+            }
+
+            if (isChecked(++uid)) {
+                $row.querySelector("td:nth-child(10)").style.backgroundColor =
+                    "#f03e3e";
+                const $button = $row
+                    .querySelector("td:nth-child(10)")
                     .querySelector(
-                        '[src="/docs/2007/img/common/icon_apm_bl.gif"]'
-                    ) ||
-                $row
-                    .querySelector("td:nth-child(5)")
-                    .querySelector(
-                        '[src="/docs/2007/img/common/icon_apm_rd.gif"]'
+                        '[src="/docs/2007/img/common/icon_wait.gif"]'
                     );
 
-            if ($button) {
-                $button.closest("a").click();
-                sessionStorage.removeItem("macro_start");
-                chrome.extension.sendMessage({ type: "successTicketing" });
-                break;
+                if ($button) {
+                    $button.closest("a").click();
+                    sessionStorage.removeItem("macro_start");
+                    chrome.extension.sendMessage({ type: "successTicketing" });
+                    break;
+                }
             }
         }
-
-        if (isChecked(++uid)) {
-            $row.querySelector("td:nth-child(6)").style.backgroundColor =
-                "#f03e3e";
-            const $button =
-                $row
-                    .querySelector("td:nth-child(6)")
-                    .querySelector(
-                        '[src="/docs/2007/img/common/icon_apm_bl.gif"]'
-                    ) ||
-                $row
-                    .querySelector("td:nth-child(6)")
-                    .querySelector(
-                        '[src="/docs/2007/img/common/icon_apm_rd.gif"]'
-                    );
-
-            if ($button) {
-                $button.closest("a").click();
-                sessionStorage.removeItem("macro_start");
-                chrome.extension.sendMessage({ type: "successTicketing" });
-                break;
-            }
-        }
-
-        if (isChecked(++uid)) {
-            $row.querySelector("td:nth-child(10)").style.backgroundColor =
-                "#f03e3e";
-            const $button = $row
-                .querySelector("td:nth-child(10)")
-                .querySelector('[src="/docs/2007/img/common/icon_wait.gif"]');
-
-            if ($button) {
-                $button.closest("a").click();
-                sessionStorage.removeItem("macro_start");
-                chrome.extension.sendMessage({ type: "successTicketing" });
-                break;
-            }
-        }
-    }
-
-    setTimeout(reload, 1000);
+        // inqSchedule();
+        reload();
+    });
 };
 
 const reload = () => {
-    document.querySelector(".btn_inq > a").click();
+    // document.querySelector(".btn_inq > a").click();
+    inqSchedule();
 };
 
 const saveCheckboxState = () => {
